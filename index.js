12d24fe7bac26a7731da4309d0009abbfdfec545
kienquoc2102 <kienquoc2102bmt@gmail.com>
2024-12-18 10:43:15 +0700

    update server deploy

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = 8000;
-server.listen(PORT,()=> {
-    console.log(`Server is running on port ${PORT}`);
-});
-// module.exports = app;
+// const PORT = 8000;
+// server.listen(PORT,()=> {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+module.exports = server;
